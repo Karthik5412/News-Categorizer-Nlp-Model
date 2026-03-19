2829f9f9f35cb793d0386cef8d50b76a2f0007fa
Karthik Kurucharlapati <155161592+Karthik5412@users.noreply.github.com>
2026-03-19 21:36:37 +0530

    Just Started

---
+++ app.py
@@ -0,0 +1,28 @@
+import streamlit as st 
+import joblib
+import pandas as pd
+from transformers import Preprocessing
+
+st.set_page_config(page_title='today news', page_icon='🗞️', layout='wide')
+
+pipeline = joblib.load('Pipeline.plk')
+le = joblib.load('Encoder.plk')
+
+st.title('Todays News')
+
+headline = st.text_input('Enter Headline')
+content = st.text_area('Enter Content')
+
+data = {
+    'Headline' : [headline],
+    'Content' : [content]
+}
+
+df = pd.DataFrame(data)
+
+btn = st.button('Predict')
+
+if btn :
+    pred = pipeline.predict(df)
+    
+    st.success(le.inverse_transform(pred))
