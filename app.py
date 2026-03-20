97d8b3131691bf5b90d87b4a9c60e6bf1fb15927
Karthik Kurucharlapati <155161592+Karthik5412@users.noreply.github.com>
2026-03-20 21:59:20 +0530

    Going on

--- app.py
+++ app.py
@@ -4,7 +4,9 @@ import pandas as pd
 from transformers import Preprocessing
 import requests
 
+api_key = '04b51110e62d2ee68a1d58b2834fc9ed'
 
+url = f'https://gnews.io/api/v4/top-headlines?category=general&lang=en&country=in&max=10&apikey={api_key}'
 
 st.set_page_config(page_title='Today news', page_icon='📰', layout='wide')
 
@@ -13,19 +15,37 @@ le = joblib.load('Encoder.plk')
 
 st.title('Todays News')
 
-headline = st.text_input('Enter Headline')
-content = st.text_area('Enter Content')
+# headline = st.text_input('Enter Headline')
+# content = st.text_area('Enter Content')
 
-data = {
-    'Headline' : [headline],
-    'Content' : [content]
-}
 
-df = pd.DataFrame(data)
+# data = {
+#     'Headline' : [headline],
+#     'Content' : [content]
+# }
 
-btn = st.button('Predict')
+data = requests.get(url).json()
 
-if btn :
-    pred = pipeline.predict(df)
+st.json(data)
+
+articles = data.get('articles', [])
+filtered_news = [
+    {
+        "Headline": a['title'], 
+        "link": a['url'], 
+        "Content" : a['content'] + ' ' + a['description'],
+        "source": a['source']['name']
+    } 
+    for a in articles
+]
+
+df = pd.DataFrame(filtered_news)
+
+# btn = st.button('Predict')
+
+
+pred = pipeline.predict(df[['Headline', 'Content']])
     
-    st.success(le.inverse_transform(pred))
+df['pred'] = le.inverse_transform(pred)
+
+st.dataframe(df)
