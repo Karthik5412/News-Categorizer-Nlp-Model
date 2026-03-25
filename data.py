2fef1cdefefa8340d642b65344d1d89aeec8040e
Karthik Kurucharlapati <155161592+Karthik5412@users.noreply.github.com>
2026-03-25 09:08:22 +0530

    Image want to inserted

--- data.py
+++ data.py
@@ -41,7 +41,13 @@ def article_links(url) :
 def UserDefinedData(url) :
     
     response = scraper.get(url)
+    soup = BeautifulSoup(response.text, 'html.parser')
     
+    content = soup.find('div', class_='artContent')
+    
+    clean_text = content.get_text(separator=' ', strip=True)
+    
+    text = clean_text[:1500]
     article = Article(url)
     
     article.html = response.text
@@ -51,7 +57,7 @@ def UserDefinedData(url) :
     
     data = {
         'Headline' : article.title,
-        'Content' : article.text,
+        'Content' : str(text),
         'Image' : article.top_image
     }
     
@@ -61,12 +67,14 @@ url = "https://www.hindustantimes.com/"
 
 url_links = article_links(url)
 
-data = {}
-for i in range (len(url_links)) :
-    data[i] = article_links(url_links[i])
+print(UserDefinedData(url_links[0]))
+
+# data = {}
+# for i in range (len(url_links)) :
+#     data[i] = article_links(url_links[i])
     
 
 
-for value in data.items() :
-    print(value)
+# for value in data.items() :
+#     print(value)
     
